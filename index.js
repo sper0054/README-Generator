af06233fb6ee0c5fb6481d57c817c652e9b19d65
sper0054 <teresa.sperl@gmail.com>
2022-01-28 16:16:40 -0600

    init

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 const generateMardown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
@@ -63,7 +64,15 @@ function writeToFile(fileName, data) {
 };
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    try {
+        const answers = await askQuestions();
+        generateMarkdown(answers);
+        writeToFile("README.md", generateMarkdown(answers));
+    } catch (err) {
+        console.log(err);
+    }
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   const licenseIcon = `http://img.shields.io/badge/license-${data.license}-brightgreen.svg`;
 
@@ -48,7 +35,7 @@ function generateMarkdown(data) {
   ## Questions
   \n View this project and more in Github by searching for ${data.github}.
   \n If you have questions, contact me directly at ${data.email}.
-`;
+`
 }
 
 module.exports = generateMarkdown;
