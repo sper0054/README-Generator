b708c866a02bab89d795961f521024cc3421db8d
sper0054 <teresa.sperl@gmail.com>
2022-01-28 14:56:52 -0600

    typo fix

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,6 +14,22 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+
+  ## Table of Contents
+
+  ## Installation
+
+  ## Usage
+
+  ## License
+
+  ## Contributing
+
+  ## Tests
+
+  ## Questions
+
 `;
 }
 
